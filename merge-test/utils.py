d6364c9abb3d8f1e1410def60d9aa0c58d505ebd
dan-strokin <tus54986@temple.edu>
2025-02-20 18:46:10 +0000

    change

--- merge-test/utils.py
+++ merge-test/utils.py
@@ -7,16 +7,14 @@ def add(a,b):
 
 def multiply(a,b):
     ### multiplies two numbers
-
     res = a * b
     return res
 
 def sqrt(a):
     ### calculates the square root of a number using built in math function
-    
     res = math.sqrt(a)
     return res
-    
+
 if(__name__ == "__main__"):
     print("You are into utils.py")
     result = add(10, 20)
