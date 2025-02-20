25f56b7286d634af32e2c9e95562d94a5712cfd5
dan-strokin <tus54986@temple.edu>
2025-02-20 18:24:20 +0000

    adding square root to master

--- merge-test/main.py
+++ merge-test/main.py
@@ -1,8 +1,12 @@
-from utils import add, multiply
+from utils import add, multiply, sqrt
 
 def main():
     print("You are on master branch")
     result = multiply(80.5,33)
     print("The result of multiplication is",result)
+
+    n=144
+    result = sqrt(n)
+    print("The square root of 144 =",result)
 
 main()
--- merge-test/utils.py
+++ merge-test/utils.py
@@ -1,9 +1,20 @@
+import math
+
 def add(a,b):
+    ### adds two numbers
     res = a+b
     return res
 
 def multiply(a,b):
+    ### multiplies two numbers
+
     res = a * b
     return res
 
-print("You are into utils.py")
+def sqrt(a):
+    ### calculates the square root of a number using built in math function
+    
+    res = math.sqrt(a)
+    return res
+
+# print("You are into utils.py")
